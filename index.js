abcbbd9a8d229e07625571ebfc9d922c962b4d56
Lokesh negi <lokeshnegi980@gmail.com>
2022-11-20 11:58:10 +0530

    change

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ function fixNav() {
         nav.classList.remove('active')
     }
 }
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
